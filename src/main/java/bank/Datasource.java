fd111109f46c494f0365959d3e0e0d30d6b66466
gabrielaxelhua <99215431+gabrielaxelhua@users.noreply.github.com>
2022-12-08 06:15:30 +0000

    construct account from data source

--- src/main/java/bank/Datasource.java
+++ src/main/java/bank/Datasource.java
@@ -26,7 +26,6 @@ public class Datasource {
     String sql = "select * from customers where username = ?";
     Customer customer = null;
 
-
     try(Connection connection = connect();
     PreparedStatement statement = connection.prepareStatement(sql)){
       
@@ -46,12 +45,34 @@ public class Datasource {
     return customer;
   }
 
+  public static Account getAccount(int accountId){
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try(Connection connection = connect();
+    PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setInt(1, accountId);
+
+      try(ResultSet resultSet = statement.executeQuery()){
+        account = new Account(
+        resultSet.getInt("id"),
+        resultSet.getString("type"),
+        resultSet.getDouble("balance"));  
+      }
+    }catch (SQLException e) {
+        e.printStackTrace();    
+    }
+    return account;
+  }
   public static void main(String[] args) {
     
     //check data source connection
     // connect();
 
     Customer customer = getCustomer("twest8o@friendfeed.com");
-    System.out.println(customer.getName());
+    Account account = getAccount(customer.getAccountId());
+    
+    System.out.println("Hi " + customer.getName() + ". Your balance is " + account.getBalance() + "." );
+   
   }
 }
